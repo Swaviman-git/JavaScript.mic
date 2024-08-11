feefa2cdda9fef4be867d213c33af66f523dc7e2
Swaviman-git <152131867+Swaviman-git@users.noreply.github.com>
2024-08-11 11:58:58 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 33
+let score1 = "34"
+console.log(typeof score)         //number
+console.log(typeof (score));    //number
+console.log(typeof score1)       //string
+console.log(typeof (score1))    //string
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);      //number
+console.log(valueInNumber);
+
+let score2 = "34abc"
+let valueInNumber2 = Number(score2)
+console.log(score2);
