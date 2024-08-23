2f5f59f4fe451d94d3a5e11c6a3d7c7c4ff364ad
Swaviman-git <152131867+Swaviman-git@users.noreply.github.com>
2024-08-23 05:39:55 +0000

    continuing

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Micky"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)        //${name} replaces Micky and ${repoCount} replaces 50
+
+const gameName = new String('ShinChan')
+console.log(gameName[0])        //S
+console.log(gameName.__proto__);        //{}
+console.log(gameName.length);       //8
+
+console.log(gameName.charAt(2))     //i
+console.log(gameName.indexOf ('n'));    //3
